38d1ca49259afe0e8d3a004f259c0d534d55af27
IltonS <ilton.sequeira.s@gmail.com>
2021-03-06 10:22:03 -0300

    Teste com o Helper IsWhiteSpace

--- ExParser/ExParser.dpr
+++ ExParser/ExParser.dpr
@@ -1,35 +1,34 @@
 library ExParser;
 
-{ Important note about DLL memory management: ShareMem must be the
-  first unit in your library's USES clause AND your project's (select
-  Project-View Source) USES clause if your DLL exports any procedures or
-  functions that pass strings as parameters or function results. This
-  applies to all strings passed to and from your DLL--even those that
-  are nested in records and classes. ShareMem is the interface unit to
-  the BORLNDMM.DLL shared memory manager, which must be deployed along
-  with your DLL. To avoid using BORLNDMM.DLL, pass string information
-  using PChar or ShortString parameters. }
-
 uses
   System.SysUtils,
-  System.Classes;
+  System.Classes,
+  System.Character;
 
 {$R *.res}
 
-function GetHelloMsg(): ShortString; stdcall;
-begin
-  Result := 'ExParser.dll diz: Olá Mundo!';
-end;
-
 function Parse(Ex : ShortString): Double; stdcall;
+var
+  Expression : String;
 begin
-  Writeln('Parâmetro informado: ' + Ex);
-  Writeln('Testando a função Parse retornando Pi...');
-  Result := 3.14;
+  {
+    From original note:
+      "To avoid using BORLNDMM.DLL, pass string information
+      using PChar or ShortString parameters."
+
+    To make use of String Helpers, the ShortString parameter is casted
+    into String
+
+    Helpers Examples:
+      * Expression.Chars[0]               : Char
+      * Expression.Chars[0].IsWhiteSpace  :Boolean
+  }
+  Expression := String(Ex);
+
+  Result := 0;
 end;
 
 exports
-  GetHelloMsg,
   Parse;
 
 end.
